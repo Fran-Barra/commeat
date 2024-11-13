a8a64d9e60f1bd52330854ab65b4eecb52c32a44
lucaslovaglio <lucas.lovaglio@ing.austral.edu.ar>
2024-11-13 17:58:55 -0300

    calendar

--- src/component/schedule/Calendar/index.tsx
+++ src/component/schedule/Calendar/index.tsx
@@ -1,16 +1,52 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css'; // Importar estilos básicos
 
 const MyCalendar = () => {
-    const [date, setDate] = useState(new Date());
+    const [date, setDate] = useState<Date>(new Date());
 
     return (
-        <div className="my-calendar">
+        <div className="border-0">
             <Calendar
                 onChange={(value) => setDate(value as Date)}
                 value={date}
-                calendarType="gregory" // Empieza el domingo
+                calendarType="gregory"
+                locale="en-US"
+                formatShortWeekday={(_locale, date) =>
+                    date.toLocaleDateString('en-US', { weekday: 'narrow' })
+                }
+                formatMonth={(_locale, date) =>
+                    date.toLocaleDateString('en-US', { month: 'short' })
+                }
+                prevLabel={null}
+                nextLabel={null}
+                prev2Label={null}
+                next2Label={null}
+                className="w-full text-center custom-calendar border-0 "
+                tileClassName={({ date: tileDate }) => {
+                    const isSelected = tileDate.toDateString() === date.toDateString();
+                    const isToday = tileDate.toDateString() === new Date().toDateString();
+                    const day = tileDate.getDay();
+                    const isWeekend = day === 0 || day === 6;
+                    const isDisabled = tileDate.getMonth() !== date.getMonth();
+
+                    return `h-12 w-12 flex items-center justify-center rounded-full ${
+                        isDisabled
+                            ? 'text-white bg-blue' 
+                            : isSelected
+                                ? 'bg-primary-500 text-white' 
+                                : isToday
+                                    ? 'text-primary-500 bg-white font-bold'
+                                    : isWeekend
+                                        ? 'text-gray-500'
+                                        : 'hover:bg-primary-100'
+                    }`;
+                }}
+                navigationLabel={({ date }) => (
+                    <span className="text-blue-500 font-light text-sm">
+                        {date.toLocaleDateString('en-US', { month: 'short' })}
+                    </span>
+                )}
             />
         </div>
     );
